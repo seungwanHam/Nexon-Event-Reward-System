0843fbeac8212ef65d1a383136749dfe35b32f19
Harry <harry@bzznbyd.com>
2025-05-15 13:55:59 +0900

    feat(event): Event 서비스 기본 구조 구현

---
+++ apps/event/src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { EventModule } from './event.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    EventModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: 3002,
+      },
+    },
+  );
+  
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+  }));
+  
+  await app.listen();
+  console.log('Event microservice is listening');
+}
+bootstrap(); 
